ca8bc788593c7ddcbccd9d9ca9bda357b92fdd6c
Jorge Fitzmaurice Torres <jfitzm43@lakers.mercyhurst.edu>
2017-11-01 12:48:32 -0400

    Dracula WordCloud

--- dracula.R
+++ dracula.R
@@ -1,8 +1,11 @@
 library(tidytext)
 library(dplyr)
+library(gutenbergr)
+library(wordcloud)
 
 ## sentiments
 ## get_sentiments('nrc')
+## unique(nrc$sentiment)
 ## get_sentiments('bing')
 ## get_sentiments('afinn')
 
@@ -10,3 +13,16 @@ nrc<-get_sentiments('nrc')
 
 nrc_fear<-nrc%>%
             filter(sentiment=='fear')
+
+dracula<-gutenberg_download(345)
+
+dracula_words<-dracula%>%
+                  unnest_tokens(word,text)
+
+dracula_fear_words<-inner_join(nrc_fear,dracula_words)
+
+dfw_freq<-dracula_fear_words%>%
+                        group_by(word)%>%
+                        summarize(count=n())
+
+wordcloud(dfw_freq$word,dfw_freq$count,min.freq=5)
